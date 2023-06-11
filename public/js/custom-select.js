6c78ca32d97a00bf64a35b4944971e81c41b3da5
justivan <justivan.dev@gmail.com>
2023-06-12 00:58:02 +0400

    updated hotel form

--- public/js/custom-select.js
+++ public/js/custom-select.js
@@ -1,26 +1,24 @@
-var singleFetch = new Choices("#choices-single-remote-fetch", {
-  allowHTML: false,
-  searchPlaceholderValue: "Search for an Arctic Monkeys record",
-})
-  .setChoices(function () {
-    return fetch("https://api.discogs.com/artists/391170/releases?token=QBRmstCkwXEvCjTclCpumbtNwvVkEzGAdELXyRyW")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        return data.releases.map(function (release) {
-          return { label: release.title, value: release.title };
-        });
-      });
-  })
-  .then(function (instance) {
-    instance.setChoiceByValue("Fake Tales Of San Francisco");
-  });
+// var singleFetch = new Choices("#choices-single-remote-fetch", {
+//   allowHTML: false,
+//   searchPlaceholderValue: "Search",
+// })
+//   .setChoices(function () {
+//     return fetch("https://api.discogs.com/artists/391170/releases?token=QBRmstCkwXEvCjTclCpumbtNwvVkEzGAdELXyRyW")
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         return data.releases.map(function (release) {
+//           return { label: release.title, value: release.title };
+//         });
+//       });
+//   })
+//   .then(function (instance) {
+//     instance.setChoiceByValue("Fake Tales Of San Francisco");
+//   });
 
 var multipleFetch = new Choices("#choices-multiple-remote-fetch", {
   allowHTML: false,
-  placeholder: true,
-  placeholderValue: "Pick an Strokes record",
   maxItemCount: 5,
   searchEnabled: false,
   removeItemButton: true,
